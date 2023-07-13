423a6ccfa7a4f832f9db6d3fb6f07f6c85453ff6
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-07-13 17:45:14 +0800

    background added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
-
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <>
-    <h1>
-      Welcome To Pokemon
-    </h1>
-    </>
-  )
+    return (
+        <div className="relative h-screen">
+            <div className="absolute inset-0">
+                <Image
+                    src="/index_pokemon_background.jpg"
+                    alt="background image"
+                    fill
+                />
+            </div>
+            <div className="relative z-10 flex flex-col items-center justify-center h-full">
+                <h1 className='text-2xl font-bold text-gray-200'>Next JS 13 Background Image with Tailwind CSS </h1>
+                <p className='mt-4 text-sm text-white'>lorem ipsom Next JS 13 Background Image with Tailwind CSS</p>
+            </div>
+        </div>
+    );
 }
+
